e0b9ad897b7ed7a1230919ecf31057a60a5baac9
amanda173 <amandakj173@gmail.com>
2026-09-11 13:49:00 +0100

    feat: preprocessing (text cleaning)

--- Python_NLP_Sentimentality.py
+++ Python_NLP_Sentimentality.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import random
 import spacy
+import re
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -14,7 +15,7 @@ pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 2000)
 
 # Import Data
-cols = ["Sentiment", "ID", "Date", "Flag", "User", "Text"]
+cols = ["sentiment", "ID", "date", "flag", "user", "text"]
 ds = pd.read_csv("x_tweets.csv", encoding = "latin1", header = None, names = cols)
 
 # Basic Data Exploration
@@ -22,6 +23,20 @@ print(ds)
 print(ds.info())
 
 # PREPROCESSING
+
+# Text Cleaning
+def clean_text(text):
+	text = text.lower()  # Convert all text to lowercase
+	text = re.sub(r"http\s+|www\s+|https\s+", '', text, flags=re.MULTILINE)  # Remove URL
+	text = re.sub(r'@\w+', '', text)  # Remove @mentions
+	text = re.sub(r'&amp;', '&', text)  # Fix HTML entities
+	text = re.sub(r'[^a-zA-Z\s]', '', text)  # Keep only letters/spaces
+	return text
+# Apply to dataset
+ds["clean_text"] = ds["text"].apply(clean_txt)
+print(ds)
+
+# Test/Train Split
 random.seed(123)  # Randomisation
 
 # Split train/test sets
@@ -37,3 +52,6 @@ train_ds = ds.drop(ds.index[test_idx])
 
 print(train_ds.info())
 print(test_ds.info())
+
+# WORD VECTORS
+docs = [nlp(text) for text in train_ds]
